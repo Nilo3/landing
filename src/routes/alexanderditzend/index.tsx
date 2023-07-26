28a82d56c17742ca7d5c3dac933f086c09d49d22
aditzend <alex@saia.ar>
2023-07-26 09:03:44 -0300

    logo

--- src/routes/alexanderditzend/index.tsx
+++ src/routes/alexanderditzend/index.tsx
@@ -1,6 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import AlexanderDitzend from '~/components/profiles/alexanderditzend';
+import AlexanderDitzend from '~/components/logo';
 
 
 
