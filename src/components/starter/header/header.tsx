23ba626b01fc021fa85a7f5443ee972a09bab563
Florencia <florenciauehara@gmail.com>
2023-11-09 10:43:33 -0300

    modificaciones
    Signed-off-by: Florencia <florenciauehara@gmail.com>

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -29,11 +29,13 @@ export default component$(() => {
 
 
         <div class="lg:flex lg:flex-row items-center justify-end">
-          <a href="https://www.linkedin.com/company/sociedad-argentina-de-inteligencia-artificial" class="rounded-full bg-transparent px-2 py-1 text-xs font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:bg-transparent hover:text-white lg:inline lg:text-lg lg:px-4 lg:py-2 my-2">LinkedIn</a>
-          <div class="lg:hidden lg:flex lg:flex-col lg:w-auto lg:items-center lg:justify-end mt-2">
-            <a href="https://tally.so/r/mBdlK5" target='_blank' class="rounded-full bg-transparent px-2 py-1 text-xs font-semibold text-white shadow-sm ring-1 ring-inset ring-white hover:ring-2 hover:ring-gray-300 hover:text-gray-300">Ser Parte</a>
+          <a href="https://www.linkedin.com/company/sociedad-argentina-de-inteligencia-artificial" class="rounded-full bg-transparent px-2 py-1 text-xs font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:ring-gray-300 hover:text-gray-300 lg:inline lg:text-lg lg:px-4 lg:py-2 mt-3.5 mr-2">LinkedIn</a>
+          <div class="lg:flex lg:flex-col lg:w-auto lg:items-center lg:justify-end mt-4">
+            <a href="https://tally.so/r/mBdlK5" target='_blank' class="rounded-full bg-transparent px-2 py-1 text-xs lg:text-lg font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:ring-2 hover:ring-gray-300 hover:text-gray-300 lg:px-4 lg:py-2 my-1">Ser Parte</a>
           </div>
         </div>
+
+
 
         
         {/* <div class="lg:hidden gap-x-12 flex-1 items-center justify-end"> */}
