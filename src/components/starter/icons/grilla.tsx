c17a11b030df383574edab2b553f471b2055cab1
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2024-03-09 18:25:22 -0300

    recortando codigo que no se usa

--- src/components/starter/icons/grilla.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-
-export const Grilla = component$((props) => {
-  return (
-    <div>
-      <img src={props.url} alt="Description of the image" />
-    </div>
-  );
-});
