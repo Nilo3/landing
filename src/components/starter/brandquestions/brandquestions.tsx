fa856463738be63f6cf66412a3ccb590652af7b0
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2024-03-14 08:01:20 -0300

    se agrega dossier y video a la web

--- src/components/starter/brandquestions/brandquestions.tsx
+++ src/components/starter/brandquestions/brandquestions.tsx
@@ -1,20 +1,63 @@
-import { component$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik"
+import image from "/images/dossier.jpg"
+import { Image } from '@unpic/qwik'
 
 export default component$(() => {
   return (
     <>
-      {/* ¿Qué es SAIA? */}
-      <div class="bg-white py-16 sm:py-24 md:py-32 lg:py-40 xl:py-48">
+      <div class="w-full flex justify-center">
+        <div class="border-t border-gray-200 w-1/2"></div>
+      </div>
+      <div class="bg-black py-16 sm:py-24 md:py-32 lg:py-40 -mt-24">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl lg:mx-0">
-          <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">¿Qué es SAIA?</h2>
-            <p class="mt-4 sm:mt-6 text-base sm:text-lg leading-6 sm:leading-8 text-gray-600">SAIA es una Sociedad Argentina de Inteligencia Artificial que tiene como objetivo principal promover el avance, desarrollo y aplicaciones de la inteligencia artificial en Argentina. Trabajamos en estrecha colaboración con expertos, académicos y profesionales para impulsar la innovación y difundir el conocimiento en este campo.</p>
+          <div class="flex flex-col lg:flex-row justify-between">
+            <div class="mx-auto max-w-2xl lg:mx-40 xl:mx-24 2xl:mx-0 lg:flex-1">
+              <h2 class="text-2xl ml-10 md:ml-0 sm:text-4xl font-bold tracking-tight text-white">
+                Dossier de prensa
+              </h2>
+              <div class="mt-10 md:ml-4">
+                <a
+                  href="/public/pdf/dossier-prensa.pdf"
+                  target="_blank"
+                  download="Dossier de prensa SAIA"
+                  class="rounded-full bg-transparent px-24 sm:ml-12 md:ml-0 py-2 text-md font-semibold  text-white shadow-sm ring-2 ring-inset ring-white hover:bg-white hover:text-black"
+                >
+                  Descargar
+                </a>
+              </div>
+            </div>
+            <div class="lg:flex-1 flex justify-center lg:justify-end mt-12 lg:mt-0">
+              <Image
+                src={image}
+                alt="Dossier de prensa"
+                class="rounded-2xl lg:mr-24 ml-4 md:ml-0 "
+                width="288"
+                height="288"
+              />
+            </div>
           </div>
         </div>
       </div>
 
-      
-      
+      {/* ¿Qué es SAIA? */}
+      <div class="bg-white py-16 sm:py-24 md:py-32 lg:py-40 xl:py-48">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              ¿Qué es SAIA?
+            </h2>
+            <p class="mt-4 sm:mt-6 text-base sm:text-lg leading-6 sm:leading-8 text-gray-600">
+              SAIA es una Sociedad Argentina de Inteligencia Artificial que
+              tiene como objetivo principal promover el avance, desarrollo y
+              aplicaciones de la inteligencia artificial en Argentina.
+              Trabajamos en estrecha colaboración con expertos, académicos y
+              profesionales para impulsar la innovación y difundir el
+              conocimiento en este campo.
+            </p>
+          </div>
+        </div>
+      </div>
+
       {/*}
         <div class="bg-white py-24 sm:py-32">
           <div class="mx-auto max-w-7xl px-6 lg:px-8">
@@ -31,12 +74,20 @@ export default component$(() => {
       <div class="bg-black py-16 sm:py-24 md:py-32 lg:py-40 xl:py-48">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-2xl sm:text-4xl font-bold tracking-tight text-white sm:text-6xl">¿Cómo puede SAIA ayudarme?</h2>
-            <p class="mt-4 sm:mt-6 text-base sm:text-lg leading-6 text-white">SAIA puede ayudarte brindando recursos, conocimientos y oportunidades en el ámbito de la inteligencia artificial. Organizamos eventos, conferencias y talleres donde puedes aprender sobre las últimas tendencias y prácticas en IA. Además, ofrecemos una plataforma de networking donde puedes conectarte con profesionales y expertos en el campo.</p>
+            <h2 class="text-2xl sm:text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              ¿Cómo puede SAIA ayudarme?
+            </h2>
+            <p class="mt-4 sm:mt-6 text-base sm:text-lg leading-6 text-white">
+              SAIA puede ayudarte brindando recursos, conocimientos y
+              oportunidades en el ámbito de la inteligencia artificial.
+              Organizamos eventos, conferencias y talleres donde puedes aprender
+              sobre las últimas tendencias y prácticas en IA. Además, ofrecemos
+              una plataforma de networking donde puedes conectarte con
+              profesionales y expertos en el campo.
+            </p>
           </div>
         </div>
       </div>
-
 
       {/*}
       <div class="bg-black py-24 sm:py-32">
@@ -48,19 +99,26 @@ export default component$(() => {
           </div>
       </div>
     */}
-      
+
       {/* ¿Qué hace SAIA diferente de otras organizaciones? */}
       <div class="bg-white py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">¿Qué hace SAIA diferente de otras organizaciones?</h2>
-            <p class="mt-6 text-lg leading-8 text-gray-600">Lo que distingue a SAIA es nuestra estrecha relación con una fundación. Al ser parte de una fundación, tenemos una visión más amplia y un compromiso adicional con la ética y la responsabilidad social en el desarrollo y aplicación de la inteligencia artificial. Además, nuestra red de colaboradores y aliados nos permite acceder a una amplia gama de recursos y experiencias en este campo.</p>
+            <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              ¿Qué hace SAIA diferente de otras organizaciones?
+            </h2>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+              Lo que distingue a SAIA es nuestra estrecha relación con una
+              fundación. Al ser parte de una fundación, tenemos una visión más
+              amplia y un compromiso adicional con la ética y la responsabilidad
+              social en el desarrollo y aplicación de la inteligencia
+              artificial. Además, nuestra red de colaboradores y aliados nos
+              permite acceder a una amplia gama de recursos y experiencias en
+              este campo.
+            </p>
           </div>
         </div>
       </div>
-
-
-
     </>
-  );
+  )
 })
--- src/components/starter/saiaconf/saiaconf.tsx
+++ src/components/starter/saiaconf/saiaconf.tsx
@@ -1,55 +1,48 @@
-import { component$ } from '@builder.io/qwik';
-import { Grilla } from '../icons/grilla';
+import { component$ } from "@builder.io/qwik"
+
 export default component$(() => {
   return (
-    <div class={['container']}>
-
-
+    <div class={["container"]}>
       <div class="container">
-        <div class="py-16 px-4 md:px-0">
-          <div class="container mx-auto">
-            <h1 class="text-white text-left text-3xl md:text-5xl  leading-tight mb-8 md:mb-12 max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl">
-              SAIAConf: La Cumbre de Innovación y Debate en <span class="font-bold">Inteligencia Artificial en Argentina</span>
-            </h1>
-            <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-              <p class="text-left text-lg leading-8">Líderes en inteligencia artificial se reunirán para moldear el futuro tecnológico del país y el mundo</p>
-              <div class="mt-10 lg:mt-0 lg:grid lg:grid-cols-2 lg:gap-12 lg:items-center lg:justify-start">
-                <a href="https://saia.ar/conf" target='_blank' class="rounded-full bg-transparent mt-5 px-5 py-3 text-md text-center font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:bg-white hover:text-black">Reservar gratis un lugar</a>
+        <div class="flex flex-row ">
+          <div class="md:py-16 md:px-0">
+            <div class="mx-auto">
+              <h1 class="text-white text-left lg:-mr-12 text-3xl md:text-5xl font-bold leading-tight mb-8 md:mb-12 max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-2">
+                Conectando al ecosistema IA
+              </h1>
+              <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+                <p class="text-left text-lg leading-8">
+                  La Sociedad Argentina de Inteligencia Artificial es una
+                  organización independiente, sin fines de lucro dedicada a
+                  hacer investigaciones, generar encuentros, fortalecer los
+                  nuevos medios de educación y conscientizar a la sociedad sobre
+                  el desarrollo ético de la IA.
+                </p>
+                <div class="hidden mt-10 lg:flex lg:gap-x-12 lg:flex-1 lg:items-center lg:justify-start">
+                  <a
+                    href="https://tally.so/r/mBdlK5"
+                    target="_blank"
+                    class="rounded-full bg-transparent px-5 py-3 w-60 text-md font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:bg-white hover:text-black"
+                  >
+                    Ser Parte
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Grilla url="https://assets.saia.ar/saia/conf/grilla.png" />
-
-      </div>
-      {/*}
-      <div class=" py-16 px-4 md:px-0">
-        <div class="container mx-auto">
-          <h1 class="text-white text-left text-3xl md:text-5xl font-bold leading-tight mb-8 md:mb-12 max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl">
-            Conectando al ecosistema IA
-          </h1>
-          <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-            <p class="text-left text-lg leading-8">La Sociedad Argentina de Inteligencia Artificial es una organización independiente, sin fines de lucro dedicada a hacer investigaciones, generar encuentros, fortalecer los nuevos medios de educación y conscientizar a la sociedad sobre el desarrollo ético de la IA.</p>
-            <div class="hidden mt-10 lg:flex lg:gap-x-12 lg:flex-1 lg:items-center lg:justify-start">
-              <a href="https://tally.so/r/mBdlK5" target='_blank' class="rounded-full bg-transparent px-5 py-3 text-md font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:bg-white hover:text-black">Ser Parte</a>
-            </div>
+          <div class="hidden lg:block mt-44 lg:ml-12 lg:mt-60 xl:mt-24 xl:ml-8">
+            <iframe
+              class="rounded-lg"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/T6PjcjKL7yQ?autoplay=1&mute=1"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
-
         </div>
-
-
-
       </div>
-
     </div>
-    */}
-
-    </div>
-
-
-
-
-
-
-  );
-});
+  )
+})
